347fe98a45d5e46ed3d1304b933184074519f70e
giansalex <giansalex@gmail.com>
2018-03-01 16:48:53 -0500

    complete example

--- src/main.go
+++ src/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/tyler-sommer/stick"
+	"os"
 )
 
+const Template string = "Hello, {{ name }}!\n"
+
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("---Starting---")
+	defer fmt.Println("---Ending---")
+
+	env := stick.New(nil)
+	env.Execute(Template, os.Stdout, map[string]stick.Value{"name": "Giancarlos"})
 }
